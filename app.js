7e981dd6e133d5dbb71850e38f0d81cd148c00fd
Jane Smith <example@email.com>
2018-04-10 14:41:46 -0700

    Re-wrote code due to error with previous loop,created and array, new loops and math function. Now generating random numbers

--- app.js
+++ app.js
@@ -1,82 +1,61 @@
 'use strict';
-//First Store
-var salmonCookiesPike = {
-  location : '1st and pike',
-  minimumCust: 23,
-  maximumCust: 65,
-  average: 6.3,
-  custEachHour: [],
-  getRandomNumber: function() {
-    return Math.floor(Math.random() * (this.maximumCust - this.minimumCust)) + this.minimumCust;
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// Start first location.
+var firstPike = {
+  minHourlyCust: 23,
+  maxHourlyCust: 65,
+  avgCookiesCust: 6.3,
+  custPerHour: [],
+  soldPerHour: [],
+  totalSold: 0,
+
+  randCustPerHour: function () {
+    for (var i = 0; i < storeHours.length; i++) {
+      var custPerHourInt = Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust;
+      custPerHourInt = Math.round(custPerHourInt);
+      this.custPerHour.push(custPerHourInt);
+    }
+  },
+
+  cookiesPerHour: function () {
+    var avgCookiesCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      avgCookiesCalc = Math.floor(this.custPerHour[i] * this.avgCookiesCust);
+      this.soldPerHour.push(avgCookiesCalc);
+    }
+  },
+
+  totalCookiesSold: function () {
+    var cookiesSoldCalc = 0;
+    for (var i = 0; i < storeHours.length; i++) {
+      cookiesSoldCalc += this.soldPerHour[i];
+    }
+    this.totalSold = cookiesSoldCalc;
+  },
+
+  renderSales: function () {
+    // Access the parent element from the DOM.
+    var ulElem = document.getElementById('first-pike');
+    for (var i = 0; i < storeHours.length; i++) {
+      // 1. Create element.
+      var listItemElem = document.createElement('li');
+      // 2. Give it content.
+      listItemElem.innerHTML = storeHours[i] + ':<br> ' + this.soldPerHour[i];
+      //3. Append it to the DOM.
+      ulElem.appendChild(listItemElem);
+    }
   }
 };
-for (var i =15; i > 0; i --) {
-  var hour = salmonCookiesPike. getRandomNumber();
-  salmonCookiesPike.custEachHour.push(hour);
+function callFirstPike() {
+  firstPike.randCustPerHour();
+  firstPike.cookiesPerHour();
+  firstPike.totalCookiesSold();
+  firstPike.renderSales();
 }
+callFirstPike();
 
-//Second Store
-var salmonCookiesSeatac = {
-  location: 'SeaTac Airport',
-  minimumCust: 3,
-  maximumCust: 24,
-  average: 1.2,
-  custEachHour: [],
-  getRandomNumber: function () {
-    return Math.floor(Math.random() * (this.maximumCust - this.minimumCust)) + this.minimumCust;
-  }
-};
-for (var i = 15; i > 0; i--) {
-  var hour = salmonCookiesSeatac.getRandomNumber();
-  salmonCookiesSeatac.custEachHour.push(hour);
-}
-
-//Third Store
-var salmonCookiesSeattlecenter = {
-  location: 'Seattle Center',
-  minimumCust: 11,
-  maximumCust: 38,
-  average: 3.7,
-  custEachHour: [],
-  getRandomNumber: function () {
-    return Math.floor(Math.random() * (this.maximumCust - this.minimumCust)) + this.minimumCust;
-  }
-};
-for (var i = 15; i > 0; i--) {
-  var hour = salmonCookiesSeattlecenter.getRandomNumber();
-  salmonCookiesSeattlecenter.custEachHour.push(hour);
-}
-
-//Fourth Store
-var salmonCookiesCapitolhill = {
-  location: 'Capitol Hill',
-  minimumCust: 20,
-  maximumCust: 38,
-  average: 2.3,
-  custEachHour: [],
-  getRandomNumber: function () {
-    return Math.floor(Math.random() * (this.maximumCust - this.minimumCust)) + this.minimumCust;
-  }
-};
-for (var i = 15; i > 0; i--) {
-  var hour = salmonCookiesCapitolhill.getRandomNumber();
-  salmonCookiesCapitolhill.custEachHour.push(hour);
-}
-
-//Fifth Store
-var salmonCookiesAlki = {
-  location: 'Alki',
-  minimumCust: 2,
-  maximumCust: 16,
-  average: 4.6,
-  custEachHour: [],
-  getRandomNumber: function () {
-    return Math.floor(Math.random() * (this.maximumCust - this.minimumCust)) + this.minimumCust;
-  }
-};
-for (var i = 15; i > 0; i--) {
-  var hour = salmonCookiesAlki.getRandomNumber();
-  salmonCookiesAlki.custEachHour.push(hour);
-}
-
-
+var ulElem = document.getElementById('first-pike');
+var listItemElem = document.createElement('li');
+listItemElem.textContent = 'Total: ' + firstPike.totalSold;
+ulElem.appendChild(listItemElem);
